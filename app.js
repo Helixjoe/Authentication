75f14a66933dd19fda8a658dddfa9edef070d38f
Helixjoe <joeljohn5112@gmail.com>
2023-07-19 14:45:57 +0530

    Level 4 : bcrypt

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ require("dotenv").config();
 const { log } = require("console");
 const express = require("express");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const app = express();
+
 mongoose.connect("mongodb://127.0.0.1:27017/usersDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
@@ -30,29 +32,33 @@ app.get("/register", (req, res) => {
 })
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save()
-        .then(() => {
-            console.log('User saved successfully');
-            res.render("secrets");
-        })
-        .catch((err) => {
-            console.error(err);
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
         });
+        newUser.save()
+            .then(() => {
+                console.log('User saved successfully');
+                res.render("secrets");
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    });
 });
+
+
 
 app.post("/login", async (req, res) => {
     const userInfo = await User.findOne({ email: req.body.username });
     if (userInfo != null) {
-        if (userInfo.password === md5(req.body.password)) {
+        bcrypt.compare(req.body.password, userInfo.password, function (err, result) {
             res.render("secrets");
-        }
+        });
     }
+});
 
-})
 app.listen("3000", (req, res) => {
     console.log("Server running on 3000");
 })
